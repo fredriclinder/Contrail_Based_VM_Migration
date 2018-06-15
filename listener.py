106bfaec8b15d05c8ee1f2d89932c013b9c422cf
Fredric Linder <flinder@juniper.net>
2018-06-15 10:19:46 +0200

    Changes to be committed: 	new file:   listener.log 	modified:   listener.py

--- listener.py
+++ listener.py
@@ -9,9 +9,8 @@ import logging
 logging.basicConfig(filename="listener.log", level=logging.DEBUG)
 
 ###########################
-## V 0.1                 ##
+## V 0.2                 ##
 ###########################
-
 
 OSuser = "admin"
 OSpassword = "JNPR.lab.1"
@@ -23,91 +22,90 @@ APPpassword = "JNPR.lab.1"
 APPRESThost = "http://172.30.105.207"
 APPauth = "openstack"
 
-hostagg = []
-mydict = {}
-tester = False
-targetinstance = []
-
+targethosts = []
 ###########################
-
-
 
 app = Flask(__name__)
 
 @app.route('/host/vrouter', methods=['POST'])
 def vrouter_flow_count():
     entityId = request.json['status']['entityId']
-#    print json.dumps(entityId, indent=4, sort_keys=True)
+    alarmstate = request.json['status']['state']
 
-    hostagg = {}
-    mydict = {}
-    tester = False
-    targetinstance = []
+    if alarmstate == 'active':
 
-# Get AppFormix token
-    app = applib.Device(APPRESThost)
-    appresponse = app.auth({'username': APPuser, 'password': APPpassword, 'AuthType': APPauth})
-
-    ###########################
-    def gethosts(APPRESThost):
-    ## Get  hosts
-        hosts = APPRESThost + ':7000/appformix/controller/v2.0/' + 'hosts'
-        hostlist = app.href(hosts)
-        return hostlist
-
-    def getaggregates(APPRESThost):
-    ## Get  aggregates
-        aggregates = APPRESThost + ':7000/appformix/controller/v2.0/' + 'aggregates'
-        agglist = app.href(aggregates)
-        return agglist
-
-    def getinstances(APPRESThost):
-    ## Get  instances
-        instances = APPRESThost + ':7000/appformix/controller/v2.0/' + 'instances'
-        instlist = app.href(instances)
-        return instlist
+        hostagg = {}
 
 
 
-    hostlist = gethosts(APPRESThost)
-    instlist = getinstances(APPRESThost)
-    agglist = getaggregates(APPRESThost)
+    # Get AppFormix token
+        app = applib.Device(APPRESThost)
+        appresponse = app.auth({'username': APPuser, 'password': APPpassword, 'AuthType': APPauth})
 
-    ###########################
+        ###########################
+        def gethosts(APPRESThost):
+        ## Get  hosts
+            hosts = APPRESThost + ':7000/appformix/controller/v2.0/' + 'hosts'
+            hostlist = app.href(hosts)
+            return hostlist
 
-    ## Create dict with hostid, vmid, aggid
+        def getaggregates(APPRESThost):
+        ## Get  aggregates
+            aggregates = APPRESThost + ':7000/appformix/controller/v2.0/' + 'aggregates'
+            agglist = app.href(aggregates)
+            return agglist
 
-    for agg in agglist['data']['Aggregates']:
-#        print json.dumps(hostagg, indent=4, sort_keys=True)
-        if agg['Aggregate']['Type'] == 'instance':
-            for myinstance in instlist['data']['VmProfile']:
-#                print json.dumps(myinstance, indent=4, sort_keys=True)
-                if myinstance['VirtualMachine']['VirtualMachineId'] in agg['Aggregate']['ObjectList']:
-                    hostagg[myinstance['VirtualMachine']['Name']] = (agg['Aggregate']['Id'],  agg['Aggregate']['Name'], myinstance['VirtualMachine']['Name'], myinstance['VirtualMachine']['ServerId'])
-#    print json.dumps(hostagg, indent=4, sort_keys=True)
+        def getinstances(APPRESThost):
+        ## Get  instances
+            instances = APPRESThost + ':7000/appformix/controller/v2.0/' + 'instances'
+            instlist = app.href(instances)
+            return instlist
 
-    ## Find Agg in target host
-    ## Search for Agg with mambers on different hosts
-    ## Reture False, True
+        hostlist = gethosts(APPRESThost)
+        instlist = getinstances(APPRESThost)
+        agglist = getaggregates(APPRESThost)
 
-    for key, value in hostagg.items():
-        if value[3] == entityId:
-            local_aggregate = value[1]
-            logging.info('Alarm from server ' + entityId + '. Target aggregate is ' + local_aggregate)
-            print 'Alarm from server ' + entityId + '. Target aggregate is ' + local_aggregate
-            for innerkey, innervalue in hostagg.items():
-                if innervalue[3] != entityId and innervalue[1] == local_aggregate:
-                    print 'server ' + innervalue[3] + ' is not local for aggregate ' + local_aggregate
+        ###########################
+
+        ## Create dict with hostid, vmid, aggid
+
+        for agg in agglist['data']['Aggregates']:
+            if agg['Aggregate']['Type'] == 'instance':
+                for myinstance in instlist['data']['VmProfile']:
+                    if myinstance['VirtualMachine']['VirtualMachineId'] in agg['Aggregate']['ObjectList']:
+                        print json.dumps(myinstance)
+                        hostagg[myinstance['VirtualMachine']['Name']] = (agg['Aggregate']['Id'],  agg['Aggregate']['Name'], myinstance['VirtualMachine']['Name'], myinstance['VirtualMachine']['ServerId'])
 
 
-#                    if innervalue[1] == local_aggregate:
-#                        logging.info('Aggregate ' + innervalue[1] + ' found on server ' + innervalue[3])
+        ## Find Agg in target host
+        ## Search for Agg with mambers on different hosts
+        ## Reture False, True
 
-    return jsonify(targetinstance)
+        for key, value in hostagg.items():
+            if value[3] == entityId:
+                local_aggregate = value[1]
+                logging.info('Alarm from server ' + entityId + '. Target aggregate is ' + local_aggregate)
+                print 'Alarm from server ' + entityId + '. Target aggregate is ' + local_aggregate
+                for innerkey, innervalue in hostagg.items():
+                    if innervalue[3] != entityId and innervalue[1] == local_aggregate:
+                        print 'server ' + innervalue[3] + ' is not local for aggregate ' + local_aggregate
+                        targethosts.append(innervalue[3])
+        print json.dumps(targethosts)
+        return json.dumps(targethosts)
+    elif alarmstate == 'inactive':
+
+
+
+        print 'Alarm from server ' + entityId + ' is set to  ' + alarmstate
+        return jsonify(entityId)
+
+    else:
+        print 'test'
+        return jsonify('test')
 
 
 app.run(
     debug=True,
-    port=int("5563"),
+    port=int("5560"),
     host="0.0.0.0"
     )
